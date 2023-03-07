d49cf56ebc5f2afdf8867e1d76f52025904b349a
Diegoferreiradev <diegoferreiradev@gmail.com>
2023-03-07 01:12:51 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using APICiaAerea.Contexts;
+using APICiaAerea.Middlewares;
+using APICiaAerea.Services;
+using APICiaAerea.Validators.Aeronave;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<CiaAreaContext>();
+builder.Services.AddTransient<AeronaveService>();
+builder.Services.AddTransient<AdicionarAeronaveValidator>();
+builder.Services.AddTransient<AtualizarAeronaveValidator>();
+builder.Services.AddTransient<ExcluirAeronaveValidator>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseMiddleware<ValidationExceptionHandlerMiddleware>();
+
+app.Run();
